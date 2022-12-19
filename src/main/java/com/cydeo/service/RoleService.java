182c1e5ab533033284c4d6361fc1b796afe729e8
nihano <nioztekin@gmail.com>
2022-12-19 19:47:09 +0300

    adding RoleService

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -1,7 +1,17 @@
 package com.cydeo.service;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RoleService {
+    //We are not extending from JPARepository here because this is Service DTO this will be transferred to UI
+    List<RoleDTO> listAllRoles();
+    RoleDTO findById(Long Id);
 
 
-public interface RoleService extends CrudService<RoleDTO,Long>{
+
+
 }
