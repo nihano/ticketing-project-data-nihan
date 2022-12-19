a109cb42d20d4f8c96cfd2a7f87bc658f77d3d72
nihano <nioztekin@gmail.com>
2022-12-19 19:52:33 +0300

    addind UserService methods

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -1,13 +1,23 @@
 package com.cydeo.service;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
 
 import java.util.List;
 
+public interface UserService {
 
-public interface UserService extends CrudService<UserDTO,String> {
+    //controller will call this method meaning UI meaning we work with DTO not entity
+    List<UserDTO> listAllUsers();
+    //Why username parameter not id because we will use this for example in update and when we click update button we will see username not id. UI will see DTO so also USerDTO does not have id. Then, we will move the information to DB with primary key.
+    UserDTO findByUserName(String username);
+    void save(UserDTO user);
+    void deleteByUserName(String username);
 
-    List<UserDTO> findManagers();
-    List<UserDTO> findEmployees();
+
+
+
+
+
 
 }
