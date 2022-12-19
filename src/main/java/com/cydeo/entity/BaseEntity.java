155a6140c3080d82c897614552907502eee56237
nihano <nioztekin@gmail.com>
2022-12-19 19:46:01 +0300

    BaseEntity adding id

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,16 +1,19 @@
 package com.cydeo.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedSuperclass
 public class BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime insertDateTime;
     private Long insertUserId;
