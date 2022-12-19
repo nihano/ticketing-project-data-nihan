a24a7193d6031b9858d1d54fa2abf432b5591b0b
nihano <nioztekin@gmail.com>
2022-12-19 19:44:54 +0300

    adding mapper

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -1,7 +1,9 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketingProjectDataApplication {
@@ -10,4 +12,16 @@ public class TicketingProjectDataApplication {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
     }
 
+    //normally steps are adding bean in the container through @Bean annotation because we need ModelMapper obj. to convert entity to DTO
+    //create a class annotate with @Configuration (@SpringBootApplication covers @Configuration)
+    //Write a method which returns the object that you trying to add in the container
+    //Annotate these method with @Bean
+
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+
 }
+
+
