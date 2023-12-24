d2a82dd97749dc1b014b27c74f7b20887a51c510
nihano <nioztekin@gmail.com>
2023-12-25 01:50:56 +0300

    updated

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -2,6 +2,7 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.entity.Role;
+import com.cydeo.mapper.MapperUtil;
 import com.cydeo.mapper.RoleMapper;
 import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
@@ -14,10 +15,12 @@ import java.util.stream.Collectors;
 public class RoleServiceImpl implements RoleService {
     private final RoleRepository roleRepository;
     private final RoleMapper roleMapper;
+    private final MapperUtil mapperUtil;
 
-    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper, MapperUtil mapperUtil) {
         this.roleRepository = roleRepository;
         this.roleMapper = roleMapper;
+        this.mapperUtil = mapperUtil;
     }
 
 
@@ -35,9 +38,12 @@ public class RoleServiceImpl implements RoleService {
         //takes one entity and converts to DTO but we are trying to return List<RoleDTO>
         //first we can write a for loop for each role
         //second way we can use Stream
-        return roleList.stream().map(roleMapper::convertToDto).collect(Collectors.toList());
+//        return roleList.stream().map(roleMapper::convertToDto).collect(Collectors.toList());
        // return roleList; //we need to return RoleDTO this returns Role. We need to convert the entity to DTO
 
+        return roleList.stream().map(role -> mapperUtil.convert(role, new RoleDTO())).collect(Collectors.toList());
+
+//        return roleList.stream().map(role -> mapperUtil.convert(role,RoleDTO.class)).collect(Collectors.toList());
     }
 
     @Override
